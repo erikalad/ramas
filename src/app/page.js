1cf9924348e8e5c869f7685a00ab1c3d17dd0404
Erika Ladner <erikaladner@MacBook-Air-de-Erika.local>
2025-07-05 16:36:29 -0300

    conflicto prueba 2

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <h4>Hello World</h4>
+      <h5>Hello World</h5>
+      <h6>Hello World</h6>
+      <p>Hello World</p>
     </div>
   );
 }
