e0198a6e018e0b6ea775b82a5441190bd092d4d3
Erika Ladner <erikaladner@MacBook-Air-de-Erika.local>
2025-07-06 11:43:31 -0300

    conflicto

---
+++ src/app/datos.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Datos() {
+  return (
+    <div className={styles.page}>
+      <div>Datos</div>
+     
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Datos from "./datos";
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <div>Hola en vivo</div>
       <div>Conflicto 1</div>
+      <Datos />
     </div>
   );
 }
